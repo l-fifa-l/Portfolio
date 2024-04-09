e983fd720b743ab2f4b1015431ebe4b09665e87b
Madhu <93859956+madhuya@users.noreply.github.com>
2024-04-09 22:54:07 +0530

    init

--- constants/data.js
+++ constants/data.js
@@ -1,14 +1,14 @@
 const userData = {
-  name: 'Vivek Kumar',
+  name: 'Madhu Yadav',
   avatar: 'https://avatars.githubusercontent.com/u/41392917',
   githubUsername: 'l-fifa-l',
-  designation: 'Full-Stack Developer',
-  email: 'starktestic@gmail.com',
-  phone: '+91 9049872675',
+  designation: 'Business Analyst',
+  email: 'madhuyadav00444@gmail.com',
+  phone: '+91 8830440231',
   address: 'Lucknow, Uttar Pradesh, India.',
   about: {
     title:
-      "I'm a software developer that loves building digital products and web applications that impact millions of lives",
+      "I'm a Business Analyst that loves building digital products and web applications that impact millions of lives",
     description: [
       `After learning HTML and struggling with CSS and Javascript, I started making frontend, but something was missing, its soul, so i learned python and Django. That was my first stack, Since then i never stopped learning`,
       `I've been coding in React, Node.js, Express, MongoDB, Bootstrap, Tailwind, C, C++, Rust, Although I barely know the syntax (Psst, Stack overflow!).`,
